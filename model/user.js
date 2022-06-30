49cc04346232a3bf79654bbb997183e197cf05c7
Ahmet Gultekin <wolfxanta@yandex.com>
2022-06-30 02:11:11 +0100

    initial commit

---
+++ model/user.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+
+   name: String,
+   email: {
+       type: String,
+       unique: true,
+       required: true
+   },
+   password: {
+       type: String,
+       required: true
+   },
+    createDate: {
+       type: Date,
+       default: Date.now
+    },
+    updateDate: Date,
+    token: String,
+    tokenExpireDate: Date
+});
+
+module.exports = mongoose.model('User', UserSchema);
